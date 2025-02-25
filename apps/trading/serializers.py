840671795051b5546e56bcd6e72176dccab3a739
Askar <w0nsdoof@gmail.com>
2025-02-26 00:30:17 +0500

    basic endpoints for trading app

--- apps/trading/serializers.py
+++ apps/trading/serializers.py
@@ -2,11 +2,16 @@ from rest_framework import serializers
 from .models import Order, Transaction
 
 class OrderSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')  # Prevent users from modifying this field
+
     class Meta:
         model = Order
-        fields = "__all__"
+        fields = ['id', 'user', 'product', 'order_type', 'quantity', 'price', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'created_at', 'updated_at']
+
 
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
-        fields = "__all__"
+        fields = ['id', 'order', 'executed_at', 'total_amount']
+
--- apps/trading/views.py
+++ apps/trading/views.py
@@ -1,17 +1,32 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+
+from django.db import transaction
+
+from apps.users.permissions import isOwner
+from apps.users.models import User
+
 from .models import Order, Transaction
 from .serializers import OrderSerializer, TransactionSerializer
 
 class OrderViewSet(viewsets.ModelViewSet):
-    queryset = Order.objects.all()
     serializer_class = OrderSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, isOwner]
 
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
+    
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
-
-class TransactionViewSet(viewsets.ModelViewSet):
+        with transaction.atomic():  
+            order = serializer.save(user=self.request.user)
+            Transaction.objects.create(order=order, total_amount=order.price * order.quantity)
+            
+class TransactionViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
     permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        if self.request.user.role == User.ADMIN:
+            return Transaction.objects.all()  
+        return Transaction.objects.filter(order__user=self.request.user)
