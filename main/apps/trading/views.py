9de6b726eefd9f950012998931e43632089df09c
Askar <w0nsdoof@gmail.com>
2025-02-26 19:32:31 +0500

    moved django code to main/

--- main/apps/trading/views.py
+++ main/apps/trading/views.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 
 from django.db import transaction
 
-from apps.users.permissions import IsOwner
+from apps.users.permissions import IsOwner, IsSalesman, IsAdmin
 from apps.users.models import User
 
 from .models import Order, Transaction
@@ -15,7 +15,16 @@ class OrderViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated, IsOwner]
 
     def get_queryset(self):
+        if self.request.user.role == User.ADMIN:
+            return Order.objects.all()  
         return Order.objects.filter(user=self.request.user)
+    
+    def get_permissions(self):
+        if self.action == 'create':
+            order_type = self.request.data.get('order_type')
+            if order_type == Order.SELL:
+                return [IsAuthenticated(), IsSalesman() | IsAdmin()]
+        return [permission() for permission in self.permission_classes]
     
     def perform_create(self, serializer):
         with transaction.atomic():  
